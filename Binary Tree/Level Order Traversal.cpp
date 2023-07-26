4d622a1e7176e5a1ecbc8b2bf0d9081bc93b931d
Jashandeep Kaur Sidhu <117754690+JashandeepSidhu712@users.noreply.github.com>
2023-07-26 19:28:05 +0530

    Update Level Order Traversal.cpp

--- Binary Tree/Level Order Traversal.cpp
+++ Binary Tree/Level Order Traversal.cpp
@@ -2,7 +2,7 @@
 #include<queue>
 using namespace std;
 
-class node
+class node //The node class represents a node in the binary tree, with integer data, and pointers to its left and right children.
 {
     public:
     int data;
@@ -22,8 +22,9 @@ node* buildTree(node* root)
     cout<<"Enter data to be inserted = ";
     cin>>data;
     root=new node(data); //dynamically allocates memory for a new Node object on the heap, initializes it with the provided data, and makes root point to this newly created node, effectively setting the root of the binary tree.
-    
-    if(data==-1)
+
+    //Base Condition
+    if(data==-1) //The function continues to build the left and right subtrees for each node until the user enters -1, indicating a NULL node.
     return NULL;
         
     cout<<"At left of ("<<root->data<<") ";
@@ -35,6 +36,7 @@ node* buildTree(node* root)
 }
 void levelOrderTraversal(node* root)
 {
+    //It uses a queue to visit each node in a breadth-first manner, printing the data of each node as it is visited.
     queue<node*> q;
     q.push(root);
     
@@ -55,9 +57,35 @@ int main()
 {
     node* root = NULL; //initializes a pointer named root to NULL, meaning it does not currently point to any memory location. In other words, the pointer root is not currently pointing to a valid node in memory, indicating an empty binary tree.
     
-    root = buildTree(root);
+    root = buildTree(root); //The buildTree function is called to construct the binary tree, starting from the root.
     
-    levelOrderTraversal(root);
+    levelOrderTraversal(root); //The levelOrderTraversal function is called to print the data of all nodes in the binary tree in level-order.
     
     return 0;
 }
+
+/*
+
+TIME COMPLEXITY
+
+buildTree Function:
+The buildTree function is used to construct a binary tree. It recursively takes input from the user and creates nodes for the tree. The time complexity of this function can be analyzed as follows:
+For each node in the binary tree, the function performs a constant amount of work (reading data from the user and creating a new node).
+Let's assume there are 'n' nodes in the binary tree.
+Since the function processes each node once, the time complexity can be approximated as O(n).
+
+levelOrderTraversal Function:
+The levelOrderTraversal function performs a level-order (breadth-first) traversal of the binary tree. It visits each node once and prints its data. The time complexity can be analyzed as follows:
+In the worst case, all nodes in the binary tree need to be traversed.
+Let's assume there are 'n' nodes in the binary tree.
+The function visits each node once, and for each node, a constant amount of work is performed (printing the node's data and enqueueing its children in the queue).
+Therefore, the time complexity of this function is O(n).
+
+main Function:
+In the main function, the buildTree function is called first, which takes O(n) time, where 'n' is the number of nodes in the binary tree.
+Overall Time Complexity:
+The time complexity of the entire program is dominated by the buildTree function, which is O(n). The levelOrderTraversal function, although also O(n), performs its operations after the binary tree is constructed.
+
+In summary, the overall time complexity of the given code is O(n), where 'n' is the number of nodes in the binary tree.
+
+*/
