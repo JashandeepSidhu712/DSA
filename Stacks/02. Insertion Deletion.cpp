815389cf98e62ea46f24555760f04ba06ca605f9
Jashandeep Kaur Sidhu <117754690+JashandeepSidhu712@users.noreply.github.com>
2023-08-27 08:28:32 +0530

    Create 02. Insertion Deletion.cpp

---
+++ Stacks/02. Insertion Deletion.cpp
@@ -0,0 +1,79 @@
+#include<iostream>
+using namespace std;
+
+class Stack
+{
+    int *st;
+    int length;
+    int top=-1;
+
+public:
+
+    Stack(int N)
+    {
+        length = N;
+        st = new int[length];
+    }
+
+    int isEmpty()  //to check if the stack is empty is not
+    {
+        if(top == -1)
+            return 1;
+        else
+            return 0;
+    }
+
+    int isFull()  //to check if the stack is full or not
+    {
+        if(top == length-1)
+            return 1;
+        else
+            return 0;
+    }
+
+    int getTop()  //to get top element of the stack
+    {
+        if(!isEmpty())
+            return(st[top]);
+        else
+            cout<<"stack empty";
+    }
+
+    void push(int value)  // to push the value in the top of the stack
+    {
+        if(isFull())
+            cout<<"stack full";
+        else
+        {
+            st[++top] = value;
+            cout<<"Value pushed: "<<value<<endl;
+            show();
+        }
+    }
+
+    int pop()   //to delete/pop value from the top of the stack
+    {
+        cout<<"top = "<<st[top]<<endl;
+        return st[top--];
+    }
+
+    void show()  //show function to view the values in the stack
+    {
+        cout<<"Stack: "<<endl;
+        for(int i=0;i<=top;i++)
+        {
+            cout<<"st["<<i<<"]"<<": "<<st[i]<<endl;
+        }
+        cout<<"\n";
+    }
+};
+int main()
+{
+    Stack obj(100);
+
+    obj.push(10);
+    obj.push(20);
+    obj.push(30);
+    obj.pop();
+    obj.show();
+}
